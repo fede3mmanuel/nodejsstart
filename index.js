abc0a0e467b783e37c6eb9bc2b081f0329a5a4f1
Fede <fede.fede.website>
2021-05-13 16:45:52 -0500

    Refactoring to callback

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const { readFile, readFileSync } = require('fs');
 
 const txt = readFileSync('./hello.txt', 'utf8');
 console.log(txt);
+
+readFile('./hello.txt', 'utf8', (err,txt) => {
+    console.log(txt);
+});
 console.log('finish line');
